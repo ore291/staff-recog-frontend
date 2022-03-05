8e1b75376f56bae599e1fd376b7c9832cb30cc4f
ore291 <oreoluwapadonu@gmail.com>
2022-03-05 10:18:51 +0100

    .

--- components/loader.jsx
+++ components/loader.jsx
@@ -1,14 +1,14 @@
 const loader = () => {
   return (
-    <div class="bg-white flex space-x-2 p-5 rounded-full justify-center items-center">
+    <div className="bg-white flex space-x-2 p-5 rounded-full justify-center items-center">
         <div
-        class="bg-blue-600 p-2  w-4 h-4 rounded-full animate-bounce blue-circle"
+        className="bg-blue-600 p-2  w-4 h-4 rounded-full animate-bounce blue-circle"
         ></div>
         <div
-        class="bg-green-600 p-2 w-4 h-4 rounded-full animate-bounce green-circle"
+        className="bg-green-600 p-2 w-4 h-4 rounded-full animate-bounce green-circle"
         ></div>
         <div
-        class="bg-red-600 p-2  w-4 h-4 rounded-full animate-bounce red-circle"
+        className="bg-red-600 p-2  w-4 h-4 rounded-full animate-bounce red-circle"
         ></div>
     </div>
   );
