3d15d558c489c1ae4674a65568e1520dfc4161c4
ore291 <oreoluwapadonu@gmail.com>
2022-04-08 09:06:15 +0100

    .

--- components/webcam.jsx
+++ components/webcam.jsx
@@ -18,8 +18,8 @@ const WebcamCapture = ({ longitude, latitude }) => {
       setLoading(true);
       try {
         const res = await axios.post(
-          // "https://facerecog.guardtrol.com/verify",
-          "http://127.0.0.1:5000/verify",
+          "https://facerecog.guardtrol.com/verify",
+          // "http://127.0.0.1:5000/verify",
           {
             img: [
               {
